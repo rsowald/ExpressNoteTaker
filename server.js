47a6b508e96c900ce9064b82fd7c27f69a794c98
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-08 21:35:20 -0800

    fixed and tested all routes

--- server.js
+++ server.js
@@ -13,23 +13,37 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const dbFilePath = path.join(__dirname, 'db', 'db.json');
 
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
 // Displays all notes
-app.get('/api/notes', (req, res) => res.json(path.join(__dirname, 'db', 'db.json')));
+app.get('/api/notes', (req, res) => {
+    res.header("Content-Type", 'application/json');
+    res.sendFile(dbFilePath);
+})
 
 // All other route inputs direct to the index page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
 
 app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFile(path.join(__dirname, 'db', 'db.json')));
+    //TODO: validate req
+    //TODO: unique id
+
+    const notesText = fs.readFileSync(dbFilePath);
+    const notes = JSON.parse(notesText);
     notes.push(req.body);
-    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('Note added!'));
+    const updatedNotes = JSON.stringify(notes, null, '\t');
+    try {
+        fs.writeFileSync(dbFilePath, updatedNotes);
+    } catch (error) {
+        console.log(error);
+    }
+    console.log('Note added!');
     res.json(req.body);
 });
 
